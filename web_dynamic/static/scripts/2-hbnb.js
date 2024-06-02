985b9583a223848cb66dd3e1c55e8d18407d3b0f
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-06-02 16:10:45 +0100

    Update API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $.get('http://' + window.location.hostname + ':5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+    } else {
+        $('#api_status').removeClass('available');
+    }
+  });
+  let selectedAmenities = {};
+
+   $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    const amenityList = Object.values(selectedAmenities).join(', ');
+    $('.amenities h4').text(amenityList);
+  })
+})

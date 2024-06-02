7933d70a0a2e1246abeb07d57498b2b44914d1b0
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-06-02 19:40:18 +0100

    Update

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -25,25 +25,25 @@ $(document).ready(function () {
 
   function fetchPlaces(data) {
     $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      url: 'http://localhost:5001/api/v1/places_search/',
       type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify({}),
       success: function (data) {
         data.forEach(place => {
-          const article = $('<article>');
-          const titleBox = $('<div>').addClass('title_box');
-          const title = $('<h2>').text(place.name);
-          const price = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
+          const article = $('<article></article>');
+          const titleBox = $('<div></div>').addClass('title_box');
+          const title = $('<h2></h2>').text(place.name);
+          const price = $('<div></div>').addClass('price_by_night').text(`$${place.price_by_night}`);
           titleBox.append(title).append(price);
 
-          const information = $('<div>').addClass('information');
-          const maxGuests = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
-          const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
-          const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
+          const information = $('<div></div>').addClass('information');
+          const maxGuests = $('<div></div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
+          const numberRooms = $('<div></div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
+          const numberBathrooms = $('<div></div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
           information.append(maxGuests).append(numberRooms).append(numberBathrooms);
 
-          const description = $('<div>').addClass('description').html(place.description);
+          const description = $('<div></div>').addClass('description').html(place.description);
           article.append(titleBox).append(information).append(description);
           $('.places').append(article);
         });

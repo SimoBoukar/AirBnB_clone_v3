e7f0172438b6882b599f4d6d0c345a436e30f6ab
Mohammed <6bouass9@gmail.com>
2024-06-02 18:29:36 +0100

    push task 5 v2.0

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         $('#api_status').removeClass('available');
     }
   });
-});
 
   let selectedAmenities = {};
 
@@ -24,34 +23,37 @@ $(document).ready(function () {
     $('.amenities h4').text(amenityList);
   });
 
-  $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({}),
-    success: function (data) {
-      data.forEach(place => {
-        const article = $('<article>');
-        const titleBox = $('<div>').addClass('title_box');
-        const title = $('<h2>').text(place.name);
-        const price = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
-        titleBox.append(title).append(price);
+  function fetchPlaces(data) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function (data) {
+        data.forEach(place => {
+          const article = $('<article>');
+          const titleBox = $('<div>').addClass('title_box');
+          const title = $('<h2>').text(place.name);
+          const price = $('<div>').addClass('price_by_night').text(`$${place.price_by_night}`);
+          titleBox.append(title).append(price);
 
-        const information = $('<div>').addClass('information');
-        const maxGuests = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
-        const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
-        const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
-        information.append(maxGuests).append(numberRooms).append(numberBathrooms);
+          const information = $('<div>').addClass('information');
+          const maxGuests = $('<div>').addClass('max_guest').text(`${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}`);
+          const numberRooms = $('<div>').addClass('number_rooms').text(`${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}`);
+          const numberBathrooms = $('<div>').addClass('number_bathrooms').text(`${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}`);
+          information.append(maxGuests).append(numberRooms).append(numberBathrooms);
 
-        const description = $('<div>').addClass('description').html(place.description);
-        article.append(titleBox).append(information).append(description);
-        $('.places').append(article);
-      });
-    }
-  });
+          const description = $('<div>').addClass('description').html(place.description);
+          article.append(titleBox).append(information).append(description);
+          $('.places').append(article);
+        });
+      }
+    });
+  }
 
   fetchPlaces({});
   $('button').click(function () {
       const amenities = Object.keys(selectedAmenities);
       fetchPlaces({ amenities: amenities });
-  })
+  });
+});
